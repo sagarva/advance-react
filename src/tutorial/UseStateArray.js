2560e8bc30e74b849d43fa80c9d4938a9e07dc22
sagarva <sagarvakumar@gmail.com>
2021-08-11 09:38:32 +0530

    Advance React

---
+++ src/tutorial/UseStateArray.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+
+function UseStateArray() {
+    return (
+        <div>
+            <h2>Use state array is in action</h2>
+            
+        </div>
+    )
+}
+
+export default UseStateArray
