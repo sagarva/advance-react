eb97a399ac8baf34e17a981a512c29c07de58620
sagarva <sagarvakumar@gmail.com>
2021-08-12 10:40:06 +0530

    JSON Demo

--- src/tutorial/LearnJSON.js
+++ src/tutorial/LearnJSON.js
@@ -2,11 +2,30 @@ import React from 'react';
 import axios from 'axios';
 
 function LearnJSON() {
+    const handleClick = (b) =>{
+        const promise = axios.post("http://localhost:4040/users",{
+            name:"Batman"},{
+                headers:{
+                    ['content-type']: 'application/json'
+                }
+        })
     
+    promise.then(response => {
+        console.log(response)
+    })
+    promise.catch(error => {
+        console.error(error);
+    })
+}
     return (
         <>
-        <h2>We are Learning JSON, Axios, REST API and CRUD Operations</h2>
-            
+        <h2>Random Title</h2>
+        <button type="button" className="btn" onClick={handleClick}>
+            Post User
+        </button>
+        {/*<button type='button' className='btn' onClick={getUsers}>
+            Get Users
+    </button>    */}
         </>
     )
 }
