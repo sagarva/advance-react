fce9e53fbacaab9ef3af16e810bd376f8750470e
sagarva <sagarvakumar@gmail.com>
2021-08-12 10:13:24 +0530

    JSON Demo

---
+++ src/tutorial/LearnJSON.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import axios from 'axios';
+
+function LearnJSON() {
+    
+    return (
+        <>
+        <h2>We are Learning JSON, Axios, REST API and CRUD Operations</h2>
+            
+        </>
+    )
+}
+
+export default LearnJSON
