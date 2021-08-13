d5ee826414b7a73fb1ff9f7e0d7e1fcdbea34bca
sagarva <sagarvakumar@gmail.com>
2021-08-13 11:51:09 +0530

    Using Hooks in React-Router-dom

--- src/tutorial/React-Router/Person.js
+++ src/tutorial/React-Router/Person.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { data } from '../data';
+import { Link, useParams } from 'react-router-dom';
 
 function Person() {
+    console.log(useParams());    //useParams() is raect-router-dom hook
+    const [name, setName] = useState("Default");
+    const {id} = useParams();
+    useEffect(() => {
+        const newPerson =data.find((person) => person.id == parseInt(id));
+        setName(newPerson.name)
+    }, [])
     return (
+        <>
         <div>
-            
+            <h1>{name}</h1>
+            <Link to="/people" className='btn'>
+                Back to People
+            </Link>
         </div>
+        </>
+        
     )
 }
 
