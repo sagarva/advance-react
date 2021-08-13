ebe13f0c812434668dd6a225dd661d193b0af69c
sagarva <sagarvakumar@gmail.com>
2021-08-13 12:41:03 +0530

    Prop-Drilling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
-import ReactRouterDom from "./tutorial/React-Router/index";
+import PropDrilling from "./tutorial/Prop-Drilling";
+//import ReactRouterDom from "./tutorial/React-Router/index";
 //import Forms from "./tutorial/Forms";
 //import LearnJSON from "./tutorial/LearnJSON";
 //import LearnJSON from "./tutorial/LearnJSON";
 
 
 function App() {
-  return ( <div className="container">
-    <ReactRouterDom />
+  return (<div className="container">
+    <PropDrilling />
 
-    </div>
+  </div>
   );
 }
 
